d0d5afaf426e6357e903affcbe8f36412cd0592a
PathakPrashantGit <prashant.r100497@gmail.com>
2023-06-10 13:21:01 +0530

    testNGFrameWork

---
+++ src/test/java/test/Day_1.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Day_1 {
+	
+	@Test
+	public void demo()
+	{
+		System.out.println("Hello");
+	}
+	@Test
+	public void demo2()
+	{
+		System.out.println("Hii GM");
+	}
+
+}
---
+++ src/test/java/test/Day_3.java
@@ -0,0 +1,25 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Day_3 {
+@Test
+public void WeblogincarLoan()
+{
+	System.out.println("Web Login car Loan");
+}
+
+@Test
+public void MobileLogincarLoan()
+{
+	System.out.println("Mobile Login car Loan");
+}
+@Test
+public void LoginAPICarLoan()
+{
+	System.out.println("API      Login car Loan");
+}
+
+
+
+}
